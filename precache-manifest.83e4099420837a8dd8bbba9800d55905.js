79ad623835b7cec44245c85c5dc651aafff520ab
sundarg4 <sundarg41995@gmail.com>
2020-11-11 15:56:20 +0100

    first commit

---
+++ precache-manifest.83e4099420837a8dd8bbba9800d55905.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "75e280b7c62e13b7c4d9",
+    "url": "/static/css/main.14595d55.chunk.css"
+  },
+  {
+    "revision": "75e280b7c62e13b7c4d9",
+    "url": "/static/js/main.aedb207e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "760cdddc0040b3517a58",
+    "url": "/static/css/2.80d41fb9.chunk.css"
+  },
+  {
+    "revision": "760cdddc0040b3517a58",
+    "url": "/static/js/2.286a0011.chunk.js"
+  },
+  {
+    "revision": "bb87d1c9c4ab1b79ecb2a045c1d3299d",
+    "url": "/static/media/CSLOGO.bb87d1c9.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ba45c8f60456a672e003a875e469d0eb",
+    "url": "/static/media/Desert.ba45c8f6.jpg"
+  },
+  {
+    "revision": "03d7fc0c5451fa27a0c317e03df52707",
+    "url": "/index.html"
+  }
+];
